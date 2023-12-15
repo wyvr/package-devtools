db488f07440c9c788241ccf4af8763ff89dcf862
wyvr <info@wyvr.dev>
2023-12-15 10:36:59 +0100

    :bento: use svg instead of emojis

--- devtools/wyvr_measure.js
+++ devtools/wyvr_measure.js
@@ -7,7 +7,7 @@ let target;
 let component;
 
 export default {
-    icon: '📈',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 3V19H21V21H3V3H5ZM20.2929 6.29289L21.7071 7.70711L16 13.4142L13 10.415L8.70711 14.7071L7.29289 13.2929L13 7.58579L16 10.585L20.2929 6.29289Z"></path></svg>',
     name: 'Measure Performance',
     description: 'get performance data from the current page',
     onMount: () => {
