cc0008856caeb79a134b831b1cbeb73d465621af
wyvr <info@wyvr.dev>
2023-12-30 01:53:29 +0100

    :sparkles: add icon method

--- devtools/wyvr_measure.js
+++ devtools/wyvr_measure.js
@@ -1,3 +1,4 @@
+import { add_icons } from './wyvr_devtools_helper/add_icons.js';
 import Toolbar from './wyvr_measure.svelte';
 
 // https://stackoverflow.com/questions/16808486/explanation-of-window-performance-javascript
@@ -5,6 +6,8 @@ import Toolbar from './wyvr_measure.svelte';
 let is_active = !!sessionStorage.getItem('wyvr_measure');
 let target;
 let component;
+
+add_icons();
 
 export default {
     icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 3V19H21V21H3V3H5ZM20.2929 6.29289L21.7071 7.70711L16 13.4142L13 10.415L8.70711 14.7071L7.29289 13.2929L13 7.58579L16 10.585L20.2929 6.29289Z"></path></svg>',
