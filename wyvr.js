28c44b8786605362cf2570eddf0b46cd2b9263a4
wyvr <info@wyvr.dev>
2023-12-30 01:51:53 +0100

    :sparkles: add remixicons

--- wyvr.js
+++ wyvr.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+    assets: [
+        { src: './node_modules/remixicon/fonts/remixicon.css', target: 'devtools/remixicon/remixicon.css' },
+        { src: './node_modules/remixicon/fonts/remixicon.ttf', target: 'devtools/remixicon/remixicon.ttf' },
+        { src: './node_modules/remixicon/fonts/remixicon.woff', target: 'devtools/remixicon/remixicon.woff' },
+        { src: './node_modules/remixicon/fonts/remixicon.woff2', target: 'devtools/remixicon/remixicon.woff2' },
+    ],
+};
